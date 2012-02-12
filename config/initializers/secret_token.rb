c9251b83b3e9feff971930fdc0e34477f934ba56
Fabio Costa <fabio.costa@deltasigma.com.br>
2012-02-12 21:40:16 -0200

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECC::Application.config.secret_token = '044aa496efc7ab209ae946e302162e95fcf684e1ff2fb8992546c63a584ee6effc52f3b12e23bc23248b5c9f2910338f841be9f9c1800b54b0ac2fae9f03ed99'
